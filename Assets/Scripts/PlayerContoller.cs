ba4e62e019b1aac4be4537429f9928782672e13a
fallingsappy <fallingsappy@users.noreply.github.com>
2021-02-23 12:05:32 +0300

    RollABall

---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moverHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moverHorizontal, 0, moverVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 10)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
